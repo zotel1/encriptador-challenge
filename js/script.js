a460bcba4fcf22a7e56edc771d72753b24a3b99e
zotel1 <zotelsigel@gmail.com>
2024-07-09 00:35:21 -0300

    update js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,46 @@
 function encriptar() {
-    var texto = document.getElementById("inputTexto").ariaValueMax.toLowerCase();
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
     // i es para que le aferte tanto a mayusculas como a minusculas
     // g es para que afecte toda la linea u oracion
     // m es para que afecte a multiples lineas o parrafos
-    var txtCifrado = texto.replace(/e/i, "enter");
-    var txtCifrado = txtCifrado.replace(/o/i, "ober");
-    var txtCifrado = txtCifrado.replace(/i/i, "imes");
-    var txtCifrado = txtCifrado.replace(/a/i, "ai");
-    var txtCifrado = txtCifrado.replace(/u/i, "ufat");
+    var txtCifrado = texto.replace(/e/igm, "enter");
+    var txtCifrado = txtCifrado.replace(/o/igm, "ober");
+    var txtCifrado = txtCifrado.replace(/i/igm, "imes");
+    var txtCifrado = txtCifrado.replace(/a/igm, "ai");
+    var txtCifrado = txtCifrado.replace(/u/igm, "ufat");
 
-    document.getElementById("imgDerecha").style.display = none;
-    document.getElementById("texto").style.display = none;
+    document.getElementById("imgDerecha").style.display = "none";
+    document.getElementById("texto").style.display = "none";
     document.getElementById("texto2").innerHTML = txtCifrado;
     document.getElementById("copiar").style.display = "show";
     document.getElementById("copiar").style.display = "inherit";
 }
+
+function desencriptar() {
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    // i es para que le aferte tanto a mayusculas como a minusculas
+    // g es para que afecte toda la linea u oracion
+    // m es para que afecte a multiples lineas o parrafos
+    var txtCifrado = texto.replace(/enter/igm, "e");
+    var txtCifrado = txtCifrado.replace(/ober/igm, "o");
+    var txtCifrado = txtCifrado.replace(/imes/igm, "i");
+    var txtCifrado = txtCifrado.replace(/ai/igm, "a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm, "u");
+
+    document.getElementById("imgDerecha").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function copiar() {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    navigator.clipboard.writeText(contenido.value)
+        .then(() => {
+            alert("¡Se copió!");
+        })
+        .catch(err => {
+            console.error('Error al copiar: ', err);
+        });}
